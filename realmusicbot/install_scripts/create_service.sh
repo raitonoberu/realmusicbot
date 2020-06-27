8ce567b68ca5ee82eb209bf58728cebc75ff4567
Denis <64320078+raitonoberu@users.noreply.github.com>
2020-06-27 16:13:10 +0700

    installation fix

--- realmusicbot/install_scripts/create_service.sh
+++ realmusicbot/install_scripts/create_service.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 SYSTEMD_SCRIPT_DIR=$( cd  $(dirname "${BASH_SOURCE:=$0}") && pwd)
+mkdir -p ~/.config/systemd/user
 cp -f "$SYSTEMD_SCRIPT_DIR/realmusicbot.service" ~/.config/systemd/user
 
 systemctl --user daemon-reload
